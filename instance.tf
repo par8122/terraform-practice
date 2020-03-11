47052f4af5a92d3b2120973f745d4db0da45a71b
parveen <par8122@gmail.com>
2020-03-11 16:36:38 -0500

    grerbv

--- instance.tf
+++ instance.tf
@@ -4,7 +4,13 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+<<<<<<< HEAD
 resource "aws_instance" "example" {
   ami           = "ami-0d729a60"
+=======
+
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-08bc77a2c7eb2b1da"
+>>>>>>> f9db034... add content
   instance_type = "t2.micro"
 }
