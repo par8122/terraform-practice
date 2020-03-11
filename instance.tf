8664bd1516eab90f37e1d8a564d8b42a475369d0
par8122 <59494737+par8122@users.noreply.github.com>
2020-03-11 16:44:48 -0500

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,9 +1,8 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
+  access_key = "AKIA3FKORL7HR7ZIW4VY"
+  secret_key = "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"
   region     = "us-east-1"
 }
-
 
 resource "aws_instance" "ubuntu" {
   ami           = "ami-08bc77a2c7eb2b1da"
