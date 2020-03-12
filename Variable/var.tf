ab04546fb2c7b9c4d44224e67e2c076fce33960d
par8122 <59494737+par8122@users.noreply.github.com>
2020-03-12 15:32:52 -0500

    Update var.tf

--- Variable/var.tf
+++ Variable/var.tf
@@ -5,14 +5,13 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-08bc77a2c7eb2b1da"
+
   }
 }
