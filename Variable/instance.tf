941417ec01a64310fac793e36920aed150952b45
par8122 <59494737+par8122@users.noreply.github.com>
2020-03-12 15:32:16 -0500

    Update instance.tf

--- Variable/instance.tf
+++ Variable/instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "example" {
-  ami           = var.AMIS[var.AWS_REGION]
+resource "aws_instance" "ubuntu" {
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
 }
